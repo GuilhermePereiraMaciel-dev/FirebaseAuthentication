c7ab344dac30d036c4b56e0f95a651e236cdb69a
GuilhermePereiraMaciel-dev <gp090309@gmail.com>
2026-09-22 14:58:09 +0000

    ok

---
+++ Firebase/config/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD89TQn4lKYKkbCaS79r8LOP1BMgjWEZu4",
+  authDomain: "manual-firebase-c8a45.firebaseapp.com",
+  projectId: "manual-firebase-c8a45",
+  storageBucket: "manual-firebase-c8a45.firebasestorage.app",
+  messagingSenderId: "206490760510",
+  appId: "1:206490760510:web:598f28522cc63c493aa86f"
+};
+
+const app = initializeApp(firebaseConfig);
